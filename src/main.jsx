edb33768ac22e459d87388b9c631ddbd06ea7a94
Carlos Coronado <coronadoc899@gmail.com>
2023-12-14 01:13:32 -0300

    Final seccion 11

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-import { TodoApp } from './10-useReducer/TodoApp';
+import { MainApp } from './11-useContext/MainApp';
+// import { TodoApp } from './10-useReducer/TodoApp';
 // import { Padre } from './09-tarea-memo/Padre';
 // import { CallbackHook } from './08-useCallback/CallbackHook';
 // import { MemoHook } from './07-useMemo/MemoHook';
@@ -15,10 +17,11 @@ import { TodoApp } from './10-useReducer/TodoApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './01-useState/CounterApp';
-// import './ 10-useReducer/intro-reducer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <TodoApp />
-  // </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+    <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 );
